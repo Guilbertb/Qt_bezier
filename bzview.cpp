3d33ba2f2305c46cca6f0161d837a0d8bed49d8f
guilbertb <guilbert@workboot.fr>
2018-06-14 18:16:04 +0200

    detection des cercles

--- bzview.cpp
+++ bzview.cpp
@@ -28,17 +28,34 @@ void BzView::mousePressEvent(QMouseEvent *event) //surcharge de mousePressEvent
     qDebug() << m_Points[m_NbPoints-1];
     update();  // rafraichi l'affichage
     }
+    else
+    {
+        // ici caluler si on est dans les cercles ...
+
+    }
+
+
+
 }
 
 void BzView::paintEvent(QPaintEvent *event)// surcharge de QPaintEveent
 {
     qDebug()<< "paintEvent";
     QPainter painter(this);
-    QPen pen(Qt::red,2);
+    QPen pen(Qt::red,PTWITDH);
     painter.setPen(pen);
 
     for (int i=0; i < m_NbPoints; i++)
-        painter.drawEllipse(m_Points[i],6,6);
+        painter.drawEllipse(m_Points[i],PTSIZE,PTSIZE);
+
+    // on place le bézier si on a les 4 points
+    if (m_NbPoints>=4)
+    {
+        QPainterPath path;
+        path.moveTo(m_Points[0]);
+        path.cubicTo(m_Points[1],m_Points[2],m_Points[3]);
+        painter.drawPath(path);
+    }
 
 
 }
--- bzview.h
+++ bzview.h
@@ -7,6 +7,8 @@
 #include <QDebug>
 
 #define NBPTS 4
+#define PTSIZE 6
+#define PTWITDH 2
 
 class BzView : public QWidget
 {
@@ -24,6 +26,7 @@ public slots:
     void OnEffacer();
     void OnConstruire(bool);
     void mousePressEvent(QMouseEvent *event);
+    int m_CapturedPoint=-1;
 };
 
 #endif // BZVIEW_H
